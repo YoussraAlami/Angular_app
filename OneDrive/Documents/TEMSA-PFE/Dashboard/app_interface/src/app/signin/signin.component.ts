4aed05d9a4a6c2fabc96073e385b3e2accdc811e
YoussraAlami <youssraalami21@gmail.com>
2024-03-01 14:11:40 +0100

    Initial commit

---
+++ OneDrive/Documents/TEMSA-PFE/Dashboard/app_interface/src/app/signin/signin.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+
+@Component({
+  selector: 'app-signin',
+  templateUrl: './signin.component.html',
+  styleUrls: ['./signin.component.css']
+})
+export class SigninComponent {
+  constructor(private router: Router) { }
+
+navigateToDashboard() {
+  // Perform any necessary logic
+  this.router.navigate(['/dashboard']);
+}
+
+}
+
+
+
+
